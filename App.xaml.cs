bdadb5e42fdf0b81fa68cb4f285fb3085d84c7f7
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-05-16 18:37:24 -0300

    Primeira versão
    *Precisa corrigir alguns bugs.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using maui;
+
+namespace Tarefas;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell(); // mudo totalmente minha pagina
+		// MainPage = new NavigationPage(new AppShell()); // mudo somente um frame referente ao menu
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Tarefas.Constantes;
+using Tarefas.Models;
+using Tarefas.Servicos;
+
+namespace maui;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		var tarefaServico = new DatabaseServico<Tarefa>(Db.DB_PATH);
+		var tarefa = new Tarefa()
+		{
+			Titulo = "Lavar o carro",
+			Descricao = "Pegar o carro na garagem e levar para lavar",
+			Status = Tarefas.Enums.Status.Backlog,
+			UsuarioId = UsuariosServico.Instancia().Todos()[0].Id
+		};
+
+		await tarefaServico.IncluirAsync(tarefa);
+
+		var quantidade = await tarefaServico.QuantidadeAsync();
+
+
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Cliquei aqui {count} vez - E tenho {quantidade} cadastrado no SQLite";
+		else
+			CounterBtn.Text = $"Cliquei aqui {count} vezes - E tenho {quantidade} cadastrados no SQLite";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
